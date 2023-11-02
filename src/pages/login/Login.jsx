466dcd039f5670ae111452adf24acc70ab8afeaa
Keat1256 <yongkeat1256@hotmail.com>
2023-11-03 02:34:01 +0800

    feat: update and add content to login page

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,11 +1,56 @@
-import "./login.scss"
+import { useContext, useState } from "react";
+import "./login.scss";
+import logoImage from "../../images/logo.png";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../../firebase";
+import { useNavigate } from "react-router-dom";
+import { AuthContext } from "../../context/AuthContext";
 
 const Login = () => {
-    return (
-        <div>
-            <h1 className="login">Login</h1>
-        </div>
-    );
-}
+  const [error, setError] = useState(false);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const navitage = useNavigate();
+
+  const { dispatch } = useContext(AuthContext);
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed in
+        const user = userCredential.user;
+        dispatch({ type: "LOGIN", payload: user });
+        navitage("/");
+      })
+      .catch((error) => {
+        setError(true);
+      });
+  };
+
+  return (
+    <div className="login">
+      <div>
+        <img src={logoImage} alt="Logo" className="logo" />
+        <form onSubmit={handleLogin}>
+          <input
+            type="email"
+            placeholder="email"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button type="submit">Login</button>
+          {error && <span>Wrong email or password!</span>}
+        </form>
+      </div>
+    </div>
+  );
+};
 
 export default Login;
