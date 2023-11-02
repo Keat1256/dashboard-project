1afc117080e91ba8b4ac9a44b2bf01f7a6936fba
Keat1256 <yongkeat1256@hotmail.com>
2023-11-03 05:49:15 +0800

    fix: update ReactDom depreciated code

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import { DarkModeContextProvider } from './context/darkModeContext';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import { AuthContextProvider } from "./context/AuthContext";
+import { DarkModeContextProvider } from "./context/darkModeContext";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = createRoot(document.getElementById("root"));
+
 root.render(
   <React.StrictMode>
     <DarkModeContextProvider>
-      <App />
+      <AuthContextProvider>
+        <App />
+      </AuthContextProvider>
     </DarkModeContextProvider>
-  </React.StrictMode>
+  </React.StrictMode>,
 );
