213c0b4e9d43e7fdb171432a4495b1817e2191b3
Keat1256 <yongkeat1256@hotmail.com>
2023-11-02 10:36:38 +0800

    feat: add datatable and its css

--- src/components/datatable/DataTable.jsx
+++ src/components/datatable/DataTable.jsx
@@ -1,18 +1,25 @@
 import './datatable.scss';
 import { DataGrid } from '@mui/x-data-grid';
 import { columns, rows } from '../../datatablesource';
+import { useState } from 'react';
 
 const Datatable = () => {
+    const [data,setData] = useState(rows)
+
+    const handleDelete = (id)=>{
+        setData(data.filter((item)=>item.id !== id))
+    };
+
     const actionColumn = [
         {
             field: "action",
             headName: "Action",
             width: 200,
-            renderCell:()=>{
+            renderCell:(params)=>{
                 return(
                     <div className="cellAction">
                         <div className="viewButton">View</div>
-                        <div className="deleteButton">Delete</div>
+                        <div className="deleteButton" onClick={()=>handleDelete(params.row.id)}>Delete</div>
                     </div>
                 )
             }
@@ -22,7 +29,8 @@ const Datatable = () => {
         <div className="datatable">
             <div style={{ height: 400, width: '100%' }}>
             <DataGrid
-                rows={rows}
+                className="datagrid"
+                rows={data}
                 columns={columns.concat(actionColumn)}
                 initialState={{
                 pagination: {
