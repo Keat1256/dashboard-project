5e98d839669ff94b4e44b22cd41e8e2d00bbdaae
Keat1256 <yongkeat1256@hotmail.com>
2023-11-02 10:37:47 +0800

    feat: add dark/light mode to UI

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
-import List from "./pages/list/List";
-import Home from "./pages/home/Home";
-import Single from "./pages/single/Single";
-import New from "./pages/new/New";
-import Login from "./pages/login/Login";
+import List from './pages/list/List';
+import Home from './pages/home/Home';
+import Single from './pages/single/Single';
+import New from './pages/new/New';
+import Login from './pages/login/Login';
 import {
   BrowserRouter,
   Routes,
   Route,
-} from "react-router-dom";
+} from 'react-router-dom';
+import './style/dark.scss';
+import { useContext } from 'react';
+import { DarkModeContext } from './context/darkModeContext';
 
 function App() {
+  const { darkMode } = useContext(DarkModeContext);
   return (
-    <div className="App">
+    <div className={darkMode ? "app dark" : "app"}>
       <BrowserRouter>
         <Routes>
           <Route path="/">
