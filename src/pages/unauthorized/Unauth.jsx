c80e5dd911c1c64bb19949fe47644aa1e8df6f04
Keat1256 <yongkeat1256@hotmail.com>
2023-11-08 00:09:28 +0800

    feat: add sidebar and navbar for unauthorized page

--- src/pages/unauthorized/Unauth.jsx
+++ src/pages/unauthorized/Unauth.jsx
@@ -1,11 +1,13 @@
-import './unauth.scss'
+import "./unauth.scss";
 import Sidebar from "../../components/sidebar/Sidebar";
 import Navbar from "../../components/navbar/Navbar";
 
 const Unauthorized = () => {
   return (
     <div>
+      <Sidebar />
       Not Authorized
+      <Navbar />
     </div>
   );
 };
