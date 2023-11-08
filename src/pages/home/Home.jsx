73665568fc5657788bfd0a5f7e0cda448ebb14af
Keat1256 <yongkeat1256@hotmail.com>
2023-11-09 07:40:46 +0800

    fix: remove unused code

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -22,10 +22,6 @@ const Home = () => {
           <Chart />
           <Featured />
         </div>
-        <div className="listContainer">
-          <div className="listTitle">Latest Transactions</div>
-          <List />
-        </div>
       </div>
     </div>
   );
