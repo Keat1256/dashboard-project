1c0f8d72222c53213c33ed67d17765d5996437a5
Keat1256 <yongkeat1256@hotmail.com>
2023-11-09 03:24:03 +0800

    feat: edit user can now update to firebase

--- src/pages/edituser/EditUser.jsx
+++ src/pages/edituser/EditUser.jsx
@@ -2,18 +2,17 @@ import "./edituser.scss";
 import { useState, useEffect } from "react";
 import Sidebar from "../../components/sidebar/Sidebar";
 import Navbar from "../../components/navbar/Navbar";
-import Chart from "../../components/chart/Chart";
-import List from "../../components/table/Table";
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import { db } from "../../firebase";
-import { doc, getDoc } from "firebase/firestore";
+import { doc, getDoc, updateDoc } from "firebase/firestore";
 import profileIcon from "../../images/pfpicon.png";
 import DriveFolderUploadOutlinedIcon from "@mui/icons-material/DriveFolderUploadOutlined";
 import { userUpdate } from "../../formSource";
+import { toast } from "react-toastify";
 
 const EditUser = ({ title }) => {
   const { userId } = useParams();
-
+  const navigate = useNavigate();
   const [userData, setUserData] = useState(null);
 
   useEffect(() => {
@@ -41,9 +40,27 @@ const EditUser = ({ title }) => {
   };
 
   // Define a handler function for form submission
-  const handleAdd = (e) => {
+  const handleAdd = async (e) => {
     e.preventDefault();
     // Add code to submit the updated user data to your database
+    if (userData && userId) {
+      try {
+        const userDocRef = doc(db, "users", userId);
+        // Update the document with the new user data
+        await updateDoc(userDocRef, userData);
+        toast.success("User info updated");
+        // You can also navigate to another page or perform other actions here if needed.
+      } catch (error) {
+        console.error("Error updating user data:", error);
+        toast.error("Error updating user info");
+        // Handle any errors that occur during the update.
+      }
+    }
+    navigate(-1);
+  };
+
+  const goBack = () => {
+    navigate(-1); // This will navigate back to the previous page
   };
 
   return (
@@ -54,12 +71,15 @@ const EditUser = ({ title }) => {
         <div className="top">
           <h1>{title}</h1>
         </div>
+        <button className="goBack" onClick={goBack}>
+          Go Back
+        </button>
         <div className="bottom">
           <div className="left">
             <img
-              src={userData.img ? userData.img : profileIcon}
-              alt={userData.name || "User Profile"}
-              className="item avatar" // Add "avatar" class here
+              src={userData?.img || profileIcon}
+              alt={userData?.name || "User Profile"}
+              className="item avatar"
             />
           </div>
           <div className="right">
@@ -81,13 +101,36 @@ const EditUser = ({ title }) => {
               {userUpdate.map((input) => (
                 <div className="formInput" key={input.id}>
                   <label>{input.label}</label>
-                  <input
-                    id={input.id}
-                    type={input.type}
-                    placeholder={input.placeholder}
-                    value={userData ? userData[input.id] : ""}
-                    onChange={handleInput}
-                  />
+                  {input.type === "select" ? (
+                    <select
+                      className="formInput"
+                      id={input.id}
+                      onChange={handleInput}
+                      value={
+                        userData && userData[input.id] ? userData[input.id] : ""
+                      } // Check if userData and userData[input.id] are not null
+                    >
+                      <option value="" disabled>
+                        Select {input.label}
+                      </option>
+                      {input.options.map((option, index) => (
+                        <option key={index} value={option.value}>
+                          {option.text}
+                        </option>
+                      ))}
+                    </select>
+                  ) : (
+                    <input
+                      className="formInput"
+                      id={input.id}
+                      type={input.type}
+                      placeholder={input.placeholder}
+                      onChange={handleInput}
+                      value={
+                        userData && userData[input.id] ? userData[input.id] : ""
+                      } // Check if userData and userData[input.id] are not null
+                    />
+                  )}
                 </div>
               ))}
               <button
@@ -96,7 +139,7 @@ const EditUser = ({ title }) => {
                 }
                 type="submit"
               >
-                Send
+                Update
               </button>
             </form>
           </div>
